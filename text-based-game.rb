170b1b994961cd97ce215577e0f4ed2db316236c
priscila2294 <priscilla_2294@hotmail.com>
2020-11-25 14:45:53 -0800

    initial commit

--- text-based-game.rb
+++ text-based-game.rb
@@ -1,5 +1,111 @@
 # 1. What's your name? Are you ready for an adventure?
 
+def start_game
+  'Hello, what is your name?'.each_char do |value|
+  putc value
+  sleep 0.05
+end
+  puts
+  gets.chomp
+
+end
+
+player_name = start_game
+
+def prompt_one name
+   "Hello #{name} You can see a shiny door in front of you. \n 
+  Would you like to open it? \n 
+  Type yes or no \n ".each_char do |value|
+    putc value
+    sleep 0.05
+  end
+   door_open_answer = gets.chomp 
+
+if door_open_answer.downcase == "yes" 
+  "You enter the shiny doorway and you're in a small metal room, and you see a slot machine. 
+  You reach into your pocket and find 2 shiny coins, a strange currency you have never seen.
+  They seem to fit perfectly into the slot machine. What do you do? If you decide to play, play wisely because you only have 2 coins.\n Type yes or no \n".each_char do |value|
+    putc value
+    sleep 0.02
+  end
+  slot_machine_answer = gets.chomp
+
+  else 
+    "game over, you just died".each_char do |value|
+      putc value
+      sleep 0.02
+    end
+    
+  end
+ 
+
+end
+
+
+ prompt_one player_name
+ 
+prompt_one_answer = prompt_one 
+
+def prompt_two answer
+  if slot_machine_answer == "yes"
+
+  'You see the numbers on the slot machine slide into place to say ... 1 ... 9 ... 6 ... 9. 
+  1969. 
+  The slot machine immediately turns off and stops playing music. 
+  What does this mean? Suddenly, a gust of wind blows around you and you get very dizzy. 
+  You turn around to the reflective metal wall behind you, and see yourself in the mirror... 
+  you have an afro and are wearing a peace sign necklace. 
+  Welcome to 1969, the summer of love. Would you like to exit the time machine and enter the summer of love? 
+                 ,@@@@@@@@@,
+               @@@@@@@@@@@@@@@
+              @@@@@@@@@@@@@@@@@
+              @@@@@@@@@@@@@@@@@
+               @@@@@_   _@@@@@
+               @@@| o   o |@@@
+                 @|  (_)  |@
+                  \  ___  /
+                  |\_`~`_/|
+             ____/:  `"`  :\____
+           /`}}}}}\       /{{{{{`\
+          /  {{{{{ \     / }}}}}  \
+         /   }}}}}  \.-./  {{{{{   \
+        |    {{{{{  / | \  }}}}}    |
+        |    }}}}}  \/|\/  {{{{{    |
+        |   /{{{{{   '-'   }}}}}\   |'.each_char do |value|
+   putc value
+   sleep 0.05
+
+
+   new_year_answer = gets.chomp 
+   
+ end
+else 
+  "game over, you just died".each_char do |value|
+    putc value
+    sleep 0.02
+  end
+
+end
+end
+
+prompt_two prompt_one_answer
+
+ 
+
+
+
+#  else 
+#    "game over, you just died".each_char do |value|
+#      putc value
+#      sleep 0.02
+#    end
+ 
+#  end
+
+# end
+
+
+
 # 2. Hi, (name) You can see a shiny door in front of you. Would you like to open it?
   # Y / N
 
